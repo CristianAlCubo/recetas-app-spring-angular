41b69c39293c7e5cab2294260fd4996fbbfeb00c
Cristian Cubillos <ccubillosreyes1@gmail.com>
2025-10-31 19:12:21 -0500

    feat: Añadido estado de carga al botón de exportar PDF

--- frontend/src/app/components/recipes/export-recipe-button/export-recipe-button.ts
+++ frontend/src/app/components/recipes/export-recipe-button/export-recipe-button.ts
@@ -1,24 +1,29 @@
 import { Component, inject, input } from '@angular/core';
 import { PDFService } from '../../../services/pdfservice';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-export-recipe-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './export-recipe-button.html',
   styleUrl: './export-recipe-button.css',
 })
 export class ExportRecipeButton {
   recipeId = input.required<number>();
   pdfService = inject(PDFService);
+  isLoading = false;
 
   exportRecipes() {
     console.log('Exporting recipe:', this.recipeId());
+    this.isLoading = true;
     this.pdfService.exportRecipePDF(this.recipeId()).subscribe({
       next: (response) => {
         console.log('Recipe exported:', response);
+        this.isLoading = false;
       },
       error: (error) => {
         console.error('Error exporting recipe:', error);
+        this.isLoading = false;
       },
     });
   }
